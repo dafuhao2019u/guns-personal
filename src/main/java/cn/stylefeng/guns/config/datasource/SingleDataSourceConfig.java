306b35d3cd41be7075a4eadf2e44ab53c03583a0
fengshuonan <sn93@qq.com>
2019-04-28 23:23:14 +0800

    更新单数据源配置

--- src/main/java/cn/stylefeng/guns/config/datasource/SingleDataSourceConfig.java
+++ src/main/java/cn/stylefeng/guns/config/datasource/SingleDataSourceConfig.java
@@ -21,14 +21,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
- * 多数据源配置
+ * 多数据源配置，多数据源配置因为和单数据源冲突，所以现在默认版本删除了多数据源配置
+ *
+ * 可参考 https://gitee.com/stylefeng/guns/tree/multi-datasource/
  *
  * @author stylefeng
  * @Date 2017/5/20 21:58
  */
 @Configuration
 @ConditionalOnProperty(prefix = "guns.muti-datasource", name = "open", havingValue = "false", matchIfMissing = true)
-@EnableTransactionManagement
+@EnableTransactionManagement(proxyTargetClass = true)
 @MapperScan(basePackages = {"cn.stylefeng.guns.modular.*.mapper"})
 public class SingleDataSourceConfig {
 
